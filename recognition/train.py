44ba5381d8c4b11c30faa44051c04890ee82a28a
ivan0313 <ivan0313@connect.hku.hk>
2021-09-18 18:34:57 +0800

    restructured project

--- recognition/train.py
+++ recognition/train.py
@@ -3,12 +3,14 @@ import numpy as np
 from PIL import Image
 import cv2
 import pickle
+from tqdm import tqdm
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "data")
 
 # Load the OpenCV face recognition detector Haar
-face_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(f'{BASE_DIR}/haarcascade/haarcascade_frontalface_default.xml')
 # Create OpenCV LBPH recognizer for training
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
@@ -19,11 +21,10 @@ x_train = []
 
 # Traverse all face images in `data` folder
 for root, dirs, files in os.walk(image_dir):
-    for file in files:
+    for file in tqdm(files):
         if file.endswith("png") or file.endswith("jpg"):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace("", "").upper()  # name
-            print(label, path)
 
             if label in label_ids:
                 pass
@@ -31,11 +32,10 @@ for root, dirs, files in os.walk(image_dir):
                 label_ids[label] = current_id
                 current_id += 1
             id_ = label_ids[label]
-            print(label_ids)
 
             pil_image = Image.open(path).convert("L")
             image_array = np.array(pil_image, "uint8")
-            print(image_array)
+
             # Using multiscle detection
             faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=3)
 
@@ -44,12 +44,17 @@ for root, dirs, files in os.walk(image_dir):
                 x_train.append(roi)
                 y_label.append(id_)
 
+print(f"{len(files)} image data collected")
+
+if not os.path.exists(f"{BASE_DIR}/model"):
+    os.makedirs(f"{BASE_DIR}/model")
+
 # labels.pickle store the dict of labels.
-# {name: id}  
+# {name: id}
 # id starts from 0
-with open("labels.pickle", "wb") as f:
+with open(f"{BASE_DIR}/model/labels.pickle", "wb") as f:
     pickle.dump(label_ids, f)
 
 # Train the recognizer and save the trained model.
 recognizer.train(x_train, np.array(y_label))
-recognizer.save("train.yml")
+recognizer.save(f"{BASE_DIR}/model/train.yml")
