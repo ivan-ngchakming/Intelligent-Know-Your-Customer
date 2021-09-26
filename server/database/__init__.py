0934eaf4bfaf028ae204a3d7197fda0c4212bfa8
ivan0313 <ivan0313@connect.hku.hk>
2021-09-26 20:11:48 +0800

    created database engine using sqlalchemy

---
+++ server/database/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+from sqlalchemy import create_engine
+
+from .metadata import metadata_obj
+from .tables import *
+
+
+# Collection mysql login configurations from environment variables
+host = os.environ['DATABASE_HOST']
+port = os.environ['DATABASE_PORT']
+username = os.environ['DATABASE_USER']
+password = os.environ['DATABASE_PASSWORD']
+database = os.environ['DATABASE_NAME']
+
+# Create an SQLAlchemy engine that connects to MySQL database
+engine = create_engine(f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}", echo=True, future=True)
+
+def create_all():
+    metadata_obj.create_all(engine)
---
+++ server/database/metadata.py
@@ -0,0 +1,4 @@
+from sqlalchemy import MetaData
+
+
+metadata_obj = MetaData()
