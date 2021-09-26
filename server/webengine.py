cee27586d2af3b886d0d82dbdfda7d6912e56b27
ivan0313 <ivan0313@connect.hku.hk>
2021-09-26 21:04:40 +0800

    grant all feture permission requrest

--- server/webengine.py
+++ server/webengine.py
@@ -3,7 +3,7 @@ import logging
 
 from PyQt5.Qt import QUrl
 from PyQt5.QtWebChannel import QWebChannel
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 
 from server.api import Foo, Transactions
 
@@ -15,13 +15,6 @@ class WebEngineView(QWebEngineView):
     def __init__(self):
         super().__init__()
 
-        # setup channel
-        logger.debug("Setting up channels")
-        self.channel = QWebChannel()
-        self.channel.registerObject('transactions', Transactions(self))
-        self.channel.registerObject('foo', Foo(self))
-        self.page().setWebChannel(self.channel)
-
         logger.debug(f"Loading react app in {ENVIRONMENT} mode")
         if ENVIRONMENT == 'production':
             self.load(QUrl("http://localhost:8000"))
@@ -30,5 +23,20 @@ class WebEngineView(QWebEngineView):
         else:
             raise Exception(f"Unknown environment configuration: {ENVIRONMENT}")
 
+        # setup channel
+        logger.debug("Setting up channels")
+        self.channel = QWebChannel()
+        self.channel.registerObject('transactions', Transactions(self))
+        self.channel.registerObject('foo', Foo(self))
+        self.page().setWebChannel(self.channel)
+        self.page().featurePermissionRequested.connect(self.onFeaturePermissionRequested)
+
         self.show()
         logger.debug(f"PyQt WebEngineView finished loaded")
+
+    def onFeaturePermissionRequested(self, securityOrigin, feature):
+        self.sender().setFeaturePermission(
+            securityOrigin,
+            feature,
+            QWebEnginePage.PermissionGrantedByUser
+        )
