656dd6d0892d5e51a4930af67736eb7523b9aeb3
ivan0313 <60818397+ivan0313@users.noreply.github.com>
2021-10-25 14:41:41 +0800

    added currency and Ex rates table (#38)

--- server/queries/__init__.py
+++ server/queries/__init__.py
@@ -2,11 +2,48 @@ import os
 import random
 from datetime import datetime, timedelta
 
-from . import users, models, accounts, transactions
+from . import users, models, accounts, transactions, currency
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def fill_dummy_data():
+    currencies = [
+        ('HKD', '$'),
+        ('USD', '$'),
+        ('EUR', '€'),
+        ('GBP', '£'),
+        ('CAD', '$'),
+    ]
+    currency.create(currencies)
+
+    rates = [
+        ('HKD', 'USD', 0.13),
+        ('HKD', 'EUR', 0.11),
+        ('HKD', 'GBP', 0.09),
+        ('HKD', 'CAD', 0.16),
+
+        ('USD', 'HKD', 7.77),
+        ('USD', 'EUR', 0.86),
+        ('USD', 'GBP', 0.73),
+        ('USD', 'CAD', 1.24),
+
+        ('EUR', 'USD', 1.16),
+        ('EUR', 'HKD', 9.05),
+        ('EUR', 'GBP', 0.85),
+        ('EUR', 'CAD', 1.44),
+
+        ('GBP', 'USD', 1.38),
+        ('GBP', 'EUR', 1.18),
+        ('GBP', 'HKD', 10.7),
+        ('GBP', 'CAD', 1.70),
+
+        ('CAD', 'USD', 0.81),
+        ('CAD', 'EUR', 0.69),
+        ('CAD', 'GBP', 0.59),
+        ('CAD', 'HKD', 6.29),
+    ]
+    currency.define_rates(rates)
+
     usernames = ['peter', 'kittie', 'ivan']
     user_ids = []
     account_ids = []
---
+++ server/queries/currency.py
@@ -0,0 +1,34 @@
+from sqlalchemy import text
+
+from server.database import engine
+
+
+def create(data):
+	values = ','.join(list(map(
+		lambda x: f"('{x[0]}', '{x[1]}')", data
+	)))
+
+	with engine.connect() as conn:
+		query = text(
+			f"""
+			INSERT INTO currency (currency, symbol)
+			VALUES {values}
+			"""
+		)
+		conn.execute(query)
+		conn.commit()
+
+def define_rates(data):
+	values = ','.join(list(map(
+		lambda x: f"('{x[0]}', '{x[1]}', {x[2]})", data
+	)))
+
+	with engine.connect() as conn:
+		query = text(
+			f"""
+			INSERT INTO exchange_rate (from_currency, to_currency, rate)
+			VALUES {values}
+			"""
+		)
+		conn.execute(query)
+		conn.commit()
